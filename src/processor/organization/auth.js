eb114048c61e521d57cad14dccb71040ffd78e50
eben-k <ennim.consults@gmail.com>
2019-12-19 13:06:04 +0100

    170379942(authentication)create signin endpoint: - add endpoint for signing in to workspace - add unique contraint to local users migration
    [Delivers 170379942]

--- src/processor/organization/auth.js
+++ src/processor/organization/auth.js
@@ -26,4 +26,30 @@ export const signup = async (userData) => {
   }
 };
 
-export const signin = () => 0;
+export const signin = async (userData) => {
+  try {
+    const { email, password, organizationId } = userData;
+    const model = await getDB(organizationId);
+    const user = await model.User.findOne({
+      where: {
+        email,
+      },
+    });
+    if (!user) {
+      return ('user not found');
+    }
+    const hashedPassword = user.password;
+    const validPassword = bcrypt.compareSync(password, hashedPassword);
+    if (!validPassword) {
+      return ('invalid password');
+    }
+    return user;
+  } catch (error) {
+    if (error.name === 'SequelizeValidationError') {
+      const err = Error('invalid user');
+      err.status = 500;
+      throw err;
+    }
+    throw error;
+  }
+};
